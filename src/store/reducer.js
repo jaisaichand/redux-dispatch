d279283bf5959fcdc33cb0e7c9034b57d89fbffc
JAI <t.jaisaichand4@gmail.com>
2020-12-08 17:10:20 +0530

    ...

---
+++ src/store/reducer.js
@@ -0,0 +1,18 @@
+const initialState = {
+    counter: 0
+}
+
+const reducer = (state = initialState, action) => {
+    if (action.type === 'INCREMENT') {
+        return {
+            counter: state.counter + 1
+        }
+    } else if (action.type === 'DECREMENT') {
+        return {
+            counter: state.counter - 1
+        }
+    }
+    return state;
+};
+
+export default reducer;
